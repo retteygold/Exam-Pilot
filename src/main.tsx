b06948ae0b7c16d2db18f63ebd7d769a60ba3b57
Developer <dev@example.com>
2026-03-19 13:14:20 +0500

    Add build stamp banner + JS console stamp

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+console.log('[BUILD] KidsDashboard expected header: Kids Zone v2.1')
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
