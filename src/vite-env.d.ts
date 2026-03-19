2f4984cd468f1635de68cf3550d77ded2c4c5862
Developer <dev@example.com>
2026-03-19 12:46:40 +0500

    Remove PWA type references from vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,17 +1,4 @@
 /// <reference types="vite/client" />
-/// <reference types="vite-plugin-pwa/client" />
-
-declare module 'virtual:pwa-register' {
-  export interface RegisterSWOptions {
-    immediate?: boolean
-    onNeedRefresh?: () => void
-    onOfflineReady?: () => void
-    onRegistered?: (swRegistration: ServiceWorkerRegistration | undefined) => void
-    onRegisterError?: (error: any) => void
-  }
-
-  export function registerSW(options?: RegisterSWOptions): (reloadPage?: boolean) => Promise<void>
-}
 
 interface ImportMetaEnv {
   // Add additional VITE_* env types here as needed
